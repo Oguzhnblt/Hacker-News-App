7d3cd240886964f90b99121bb58d97296fe66b02
Oguzhnblt <oguzhanbolat0@gmail.com>
2023-03-19 17:00:37 +0300

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Oğuz on 19.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
